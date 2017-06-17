405ffcdc12da59206f718f86df26b7aaedd2a7c2
Lance Turri <lance.turri@gmail.com>
2017-06-17 02:44:21 -0500

    Fixed sidebar when viewing on mobile devices, updated headers on all views.

--- hnb/Scripts/site.js
+++ hnb/Scripts/site.js
@@ -3,6 +3,11 @@ $(function () {
     // This will initiate the sidebar menu when screen size is mobile width.
     // http://materializecss.com/navbar.html
     $(".button-collapse").sideNav();
+
+    // Click binding for the donation link in the header.
+    $('#donate').on('click', function(e) {
+        $(e.currentTarget).closest('form').submit();
+    })
 });
        
 
