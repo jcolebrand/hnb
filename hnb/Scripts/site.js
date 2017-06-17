3fdb8f6aa01a8c0a99ae7eff26baf43f2390ea88
Cole Brand <cbrand@alkamitech.com>
2017-06-17 14:50:54 -0500

    Added FAQ tab, removed some verbiage, fixed up the meet page to have both shelters listed, removed unnecessary site additions for now

--- hnb/Scripts/site.js
+++ hnb/Scripts/site.js
@@ -3,11 +3,7 @@ $(function () {
     // This will initiate the sidebar menu when screen size is mobile width.
     // http://materializecss.com/navbar.html
     $(".button-collapse").sideNav();
-
-    // Click binding for the donation link in the header.
-    $('#donate').on('click', function(e) {
-        $(e.currentTarget).closest('form').submit();
-    })
+    $(".dropdown-button").dropdown({ hover: false });
 });
        
 
