ff2d77eeff74cae4e52df7980f662516bdf5abfa
Cole Brand <cbrand@alkamitech.com>
2017-06-17 12:04:50 -0500

    Add a bunch of controllers because VS Mac is weird

---
+++ hnb/Controllers/AdoptController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hnb.Controllers
+{
+    public class AdoptController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
---
+++ hnb/Controllers/MeetController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace hnb.Controllers
+{
+    public class MeetController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
