4aaebcd6bb48649e72c2f36ffa36bbbfa86ce3ad
Lance Turri <lance.turri@gmail.com>
2017-07-01 01:45:06 -0500

    Minor tweaks to the card styles.

--- hnb/Scripts/adopt.js
+++ hnb/Scripts/adopt.js
@@ -5,6 +5,7 @@ function createViewModel(response) {
     });
 }
 
+// Get our data and build our viewmodel
 $.when(
     $.ajax('http://api.adoptapet.com/search/pets_at_shelter?key=8059dcbb48596dd74e3f424368018f42&output=json&shelter_id=95658'), 
     $.ajax('http://api.adoptapet.com/search/pets_at_shelter?key=dc618ad6fe052fe942eff360409180c6&output=json&shelter_id=95500')
